9754793531e017dc2dea7c1be4a1e799a5514263
juanjo2784 <juanjo.chavez2784@gmail.com>
2026-02-06 00:17:58 +0200

    cc5

--- App.py
+++ App.py
@@ -27,7 +27,7 @@ dist, prom, blur, width = def_params
 st.sidebar.header("🕹️ Ajuste Fino")
 s_dist = st.sidebar.slider("Separación (Evita doble conteo)", 1, 150, dist)
 s_prom = st.sidebar.slider("Sensibilidad (Fuerza sombra)", 1, 100, prom)
-s_width = st.sidebar.slider("Filtro de 'Grosor' de sombra", 3.0, 10.0, width)
+s_width = st.sidebar.slider("Filtro de 'Grosor' de sombra", 5.0, 10.0, width)
 s_blur = st.sidebar.slider("Filtro de Reflejo Flash", 1, 25, blur, step=2)
 
 img_file = st.file_uploader("Sube la foto con Flash", type=['jpg', 'jpeg', 'png'])
