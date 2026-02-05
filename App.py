34c5c86075ba36a3c9ec1750081f2f03b5fc974d
juanjo2784 <juanjo.chavez2784@gmail.com>
2026-02-05 23:57:05 +0200

    cc3

--- App.py
+++ App.py
@@ -30,7 +30,7 @@ dist, prom, blur, width = def_params
 st.sidebar.header("🕹️ Calibración de la IA")
 s_dist = st.sidebar.slider("Separación (Evita doble línea)", 1, 150, dist)
 s_prom = st.sidebar.slider("Sensibilidad (Fuerza sombra)", 1, 100, prom)
-s_width = st.sidebar.slider("Filtro de 'Grosor' de sombra", 3.5, 10.0, width)
+s_width = st.sidebar.slider("Filtro de 'Grosor' de sombra", 4.0, 10.0, width)
 s_blur = st.sidebar.slider("Filtro de Reflejo Flash", 1, 25, blur, step=2)
 
 img_file = st.file_uploader("Sube la foto con Flash", type=['jpg', 'jpeg', 'png'])
@@ -69,11 +69,7 @@ if img_file is not None:
     with col_res1:
         st.subheader("Visualización")
         st.image(img_res, use_container_width=True)
-    
-    with col_res2:
-        st.subheader("Validación")
-        st.metric("Sugerencia de la IA", f"{ia_total} un")
-        
+           
         # El ajuste manual toma el valor de la IA como base
         conteo_final = st.number_input("🔢 Conteo Final (Ajuste Manual):", 
                                        min_value=0, 
